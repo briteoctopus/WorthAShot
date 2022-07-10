84614acf7abb6e2bb159a237b2d71d636428aea7
Stephanie Boggs <briteoctopus@gmail.com>
2022-07-10 01:06:49 -0400

    added form for unit coverter in html

--- worthashot.js
+++ worthashot.js
@@ -5,6 +5,7 @@ function getRandomDrink() {
     .then((response) => {
       if (response.ok) {
         document.getElementById("ingredientsList").innerHTML = "";
+        document.getElementById("amounts").innerHTML = "";
         return response.json();
       } else {
         throw new Error("NETWORK RESPONSE ERROR");
@@ -17,6 +18,7 @@ function getRandomDrink() {
     .catch((error) => console.error("FETCH ERROR:", error));
 }
 
+//grabs drink information from API & displays in DOM
 function displayDrink(data) {
   const drink = data.drinks[0];
   const drinkName = drink.strDrink;
@@ -26,6 +28,7 @@ function displayDrink(data) {
   document.getElementById("drinkImage").src = drinkPic;
   document.getElementById("instructions").innerHTML = steps;
 
+  //gets list of ingredients/displays as UL
   const ingredientList = Object.keys(drink)
     .filter(function (ingredient) {
       return ingredient.indexOf("strIngredient") == 0;
@@ -37,7 +40,16 @@ function displayDrink(data) {
       return ingredients;
     }, {});
 
-/*    const measurementList = Object.keys(drink)
+  for (let key in ingredientList) {
+    let ingList = ingredientList[key];
+
+    document.getElementById("ingredientsList").innerHTML +=
+      "<li>" + ingList + "</li>";
+    document.getElementById("title").innerHTML = "Ingredients for " + drinkName;
+  }
+
+  //gets measurements for ingredients and displays as UL
+  const measurementList = Object.keys(drink)
     .filter(function (measurement) {
       return measurement.indexOf("strMeasure") == 0;
     })
@@ -46,14 +58,13 @@ function displayDrink(data) {
         measurements[measurement] = drink[measurement];
       }
       return measurements;
-    }, {});  */
-    
-    for (let key in ingredientList) {
-    let inglist = ingredientList[key];
-    //let measures = measurementList[key];
-    
-    document.getElementById("ingredientsList").innerHTML +=
-      "<li>" + inglist + "</li>";
-    document.getElementById("title").innerHTML = "Ingredients for " + drinkName;
+    }, {});
+  for (let key in measurementList) {
+    let amountList = measurementList[key];
+
+    document.getElementById("amounts").innerHTML +=
+      "<li>" + amountList + "</li>";
   }
 }
+
+//Code for conversion tool starts here
